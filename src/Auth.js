618ddfd49b9415f37ac80b77dc960b9571eb47ed
Aditya-Singh-Dabhra <ror.aditya.ad@gmail.com>
2020-05-10 22:37:48 +0530

    just a small change in callback

--- src/Auth.js
+++ src/Auth.js
@@ -7,7 +7,7 @@ class Auth {
       domain: 'dev-mrt5f3nj.auth0.com',
       audience: 'https://dev-mrt5f3nj.auth0.com/userinfo',
       clientID: 'UnxHh8iAwQdA2SIJyrHePZJDnxzwCCJM',
-      redirectUri: 'http://localhost:3000/callback',
+      redirectUri: 'https://ancient-garden-52379.herokuapp.com/callback',
       responseType: 'id_token',
       scope: 'openid profile'
     });
